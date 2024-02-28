50b01b5c3d0025c16db948967f9425ac41a82b41
Carlos Eduardo Gonzalez Henriquez <cargonzalez0601@gmail.com>
2024-02-28 16:55:16 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/cegh2001.github.io/",
+  base: "/cegh2001.github.io",
   plugins: [react()],
 })
